8032d1b74a48a0560bf4e144ecac666603214baa
Ozzy <40730209+Intresting09@users.noreply.github.com>
2026-08-26 18:11:24 -0600

    Add basic kubejs files

---
+++ minecraft/kubejs/server_scripts/remove_recipes.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+
+//Hexerei
+event.remove({ id: 'hexerei:mahogany_broom_from_mixing_cauldron'})
+event.remove({ id: 'hexerei:witch_hazel_broom_from_mixing_cauldron'})
+event.remove({ id: 'hexerei:willow_broom_from_mixing_cauldron'})
+
+})
