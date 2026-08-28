a45d89ba1098ea91d260585df2b749cb62918fcf
Galactinide <galactinide@gmail.com>
2026-08-27 22:09:20 -0700

    Merged Limestone Crushing Recipes

--- minecraft/kubejs/server_scripts/add_recipes.js
+++ minecraft/kubejs/server_scripts/add_recipes.js
@@ -1,3 +1,4 @@
+//Mahogany Broom (broken)
 ServerEvents.recipes(event => {
     event.custom({
         "type": 'hexerei:mixingcauldron',
@@ -6,39 +7,53 @@ ServerEvents.recipes(event => {
         },
         "ingredients": [
             {
-                'item': "minecraft:netherite_ingot"
+                "item": 'minecraft:netherite_ingot'
             },
             {
-                'item': "hexerei:mahogany_log"
+                "item": 'hexerei:mahogany_log'
             },
             {
-                'item': "enchanted:demonic_blood"
+                "item": 'enchanted:demonic_blood'
             },
             {
-                'tag': "mulch:mandrake"
+                "tag": 'mulch:mandrake'
             },
             {
-                'item': "enchanted:flying_ointment"
+                "item": 'enchanted:flying_ointment'
             },
             {
-                'tag': "mulch:mandrake"
+                "tag": 'mulch:mandrake'
             },
             {
-                'item': "enchanted:demonic_blood"
+                "item": 'enchanted:demonic_blood'
             },
             {
-                'item': "hexerei:mahogany_log"
+                "item": 'hexerei:mahogany_log'
             }
         ],
         "output": {
-            'item': "hexerei:mahogany_broom"
+            "item": 'hexerei:mahogany_broom'
         },
         "liquidOutput": {
-            'fluid': "hexerei:blood_fluid"
+            "fluid": 'hexerei:blood_fluid'
         },
         "fluidLevelsConsumed": 1000
     })
 
+//Create Limestone Crushing Merge
 
+    event.recipes.create.crushing(
+        [
+        'tfmg:limesand',
+        'garnished:crushed_salt',
+        'create_aquatic_ambitions:calcium_rich_powder',
+        CreateItem.of('create_aquatic_ambitions:calcium_rich_powder', 0.5),
+        CreateItem.of('create_aquatic_ambitions:suspicious_rock', 0.05),
+        CreateItem.of('minecraft:iron_nugget', 0.2),
+        CreateItem.of('create:zinc_nugget', 0.1),
+        ],
+        'create:limestone'
+    )
 
+//Closing Brackets
 })
--- minecraft/kubejs/server_scripts/remove_recipes.js
+++ minecraft/kubejs/server_scripts/remove_recipes.js
@@ -5,4 +5,10 @@ event.remove({ id: 'hexerei:mahogany_broom_from_mixing_cauldron'})
 event.remove({ id: 'hexerei:witch_hazel_broom_from_mixing_cauldron'})
 event.remove({ id: 'hexerei:willow_broom_from_mixing_cauldron'})
 
+//Create Limestone Crushing Merge
+//make sure to remove the leading "jei:/" and replace the next "/" with a ":" for recipes that start with jei
+event.remove({ id: 'create:crushing/limestone'})
+event.remove({ id: 'garnished:integration/create_aquatic_ambitions/crushed_salt_crushing'})
+
+//Closing Brackets
 })
