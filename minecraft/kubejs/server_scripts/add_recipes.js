fff405f7442e5274188ce127ac4c1e4db19482c6
Galactinide <galactinide@gmail.com>
2026-08-27 23:13:17 -0700

    added quark limestone crushing and milling

--- minecraft/kubejs/server_scripts/add_recipes.js
+++ minecraft/kubejs/server_scripts/add_recipes.js
@@ -41,19 +41,41 @@ ServerEvents.recipes(event => {
     })
 
 //Create Limestone Crushing Merge
-
+//Create Limestone
     event.recipes.create.crushing(
         [
-        'tfmg:limesand',
-        'garnished:crushed_salt',
-        'create_aquatic_ambitions:calcium_rich_powder',
-        CreateItem.of('create_aquatic_ambitions:calcium_rich_powder', 0.5),
-        CreateItem.of('create_aquatic_ambitions:suspicious_rock', 0.05),
-        CreateItem.of('minecraft:iron_nugget', 0.2),
-        CreateItem.of('create:zinc_nugget', 0.1),
+            'tfmg:limesand',
+            'garnished:crushed_salt',
+            'create_aquatic_ambitions:calcium_rich_powder',
+            CreateItem.of('create_aquatic_ambitions:calcium_rich_powder', 0.5),
+            CreateItem.of('create_aquatic_ambitions:suspicious_rock', 0.05),
+            CreateItem.of('minecraft:iron_nugget', 0.2),
+            CreateItem.of('create:zinc_nugget', 0.1),
         ],
         'create:limestone'
     )
-
+//Quark Limestone Crushing
+    event.recipes.create.crushing(
+        [
+            'tfmg:limesand',
+            'garnished:crushed_salt',
+            'create_aquatic_ambitions:calcium_rich_powder',
+            CreateItem.of('create_aquatic_ambitions:calcium_rich_powder', 0.5),
+            CreateItem.of('create_aquatic_ambitions:suspicious_rock', 0.05),
+            CreateItem.of('minecraft:iron_nugget', 0.2),
+            CreateItem.of('create:zinc_nugget', 0.1),
+        ],
+        'quark:limestone'
+    )
+//Quark Limestone Milling
+    event.recipes.create.milling(
+        [
+            'garnished:crushed_salt',
+            'create_aquatic_ambitions:calcium_rich_powder',
+            CreateItem.of('create_aquatic_ambitions:calcium_rich_powder', 0.5),
+            CreateItem.of('create_aquatic_ambitions:suspicious_rock', 0.05),
+        ],
+        'quark:limestone'
+    )
 //Closing Brackets
 })
