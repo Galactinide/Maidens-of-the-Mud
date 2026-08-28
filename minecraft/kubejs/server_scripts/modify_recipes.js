cf5e3ad550010e4e949dfa06bfb2760e598e4abc
Galactinide <galactinide@gmail.com>
2026-08-28 13:42:13 -0700

    Create modify_recipes.js

---
+++ minecraft/kubejs/server_scripts/modify_recipes.js
@@ -0,0 +1,35 @@
+ServerEvents.recipes(event =>{
+
+    //Make All Knives Unbreakable
+    const knifeunbreak = (kbknife) => {
+        event.replaceOutput(
+        {output:kbknife},
+        kbknife,
+        Item.of(kbknife).enchant('minecraft:unbreaking',10)
+        )
+        knifeunbreak('create_things_and_misc:copper_knife')
+        knifeunbreak('create_things_and_misc:zinc_knife')
+        knifeunbreak('create_things_and_misc:brass_knife')
+        knifeunbreak('farmersdelight:flint_knife')
+        knifeunbreak('farmersdelight:iron_knife')
+        knifeunbreak('farmersdelight:diamond_knife')
+        knifeunbreak('farmersdelight:netherite_knife')
+        knifeunbreak('farmersdelight:golden_knife')
+        knifeunbreak('twilightdelight:ironwood_knife')
+        knifeunbreak('twilightdelight:steeleaf_knife')
+        knifeunbreak('twilightdelight:knightmetal_knife')
+        knifeunbreak('twilightdelight:fiery_knife')
+    }
+
+
+    //{Unbreakable: 1b}
+    //make iron knife sharp (test: works)
+    /*event.replaceOutput(
+        {output:'farmersdelight:iron_knife'},
+        'farmersdelight:iron_knife',
+        Item.of('farmersdelight:iron_knife').enchant('minecraft:unbreaking',10)
+    )*/
+    
+
+//Closing Brackets
+})
