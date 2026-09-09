e11c473fdf581e37ac3c4bf2b6d37d6166d00711
Girlmossy <123712436+Beanie735@users.noreply.github.com>
2026-09-08 21:21:45 -0700

    Le Fishe Au Chocolat

--- minecraft/kubejs/startup_scripts/item_registry.js
+++ minecraft/kubejs/startup_scripts/item_registry.js
@@ -10,5 +10,21 @@ StartupEvents.registry('item', event => {
         .displayName('Sticky Glutinous Mess')
         .texture('mud:item/incomplete_glutinous_mass')
         .maxStackSize(64)
+
+    event.create('le_fishe_au_chocolat')
+        .displayName('Le Fishe au Chocolat')
+        .texture('mud:item/lefisheauchocolat')
+        .maxStackSize(2)
+        .tooltip('Le Fishe au Chocolat')
+        .food(food => {
+            food
+                .nutrition(1)
+                .saturation(0.1)
+                .effect('minecraft:glowing', 1200, 0, 1.0)
+                .effect('minecraft:nausea', 200, 1, 1.0)
+                .alwaysEdible()
+        })
+
+            
     
 })
