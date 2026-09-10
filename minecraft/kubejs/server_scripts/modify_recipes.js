087369bbb96a6b97c46218451c4a5cfa653f978a
Galactinide <galactinide@gmail.com>
2026-09-09 20:44:25 -0700

    excluded magma cream from glutinous mass usage

--- minecraft/kubejs/server_scripts/modify_recipes.js
+++ minecraft/kubejs/server_scripts/modify_recipes.js
@@ -42,9 +42,13 @@ ServerEvents.recipes(event =>{
 
     //Replace Slimeball Recipes with #c:slime_balls tag
     event.replaceInput(
-      {input: 'minecraft:slime_ball', not: {output:'minecraft:slime_block'}},
-      'minecraft:slime_ball',
-      '#c:slime_balls'
+        {
+            input: 'minecraft:slime_ball',
+            not: {output:'minecraft:slime_block'},
+            not: {output: 'minecraft:magma_cream'}
+        },
+        'minecraft:slime_ball',
+        '#c:slime_balls'
     )
 
 //Closing Brackets
